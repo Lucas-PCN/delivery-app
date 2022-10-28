776a340ac182f4c1e1c91331f2855f8184f36d4e
BiaKast <biahdrew94@gmail.com>
2022-10-27 21:43:41 -0300

    feat: cria model e migration de salesProducts

---
+++ back-end/src/database/models/salesproducts.js
@@ -0,0 +1,50 @@
+const sales = require('./sales');
+const products = require('./products');
+
+module.exports = (sequelize, DataTypes) => {
+  const SalesProducts = sequelize.define('salesProducts', {
+    saleId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: sales,
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      field: 'sale_id'
+    },
+    productId: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: products,
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+      field: 'product_id'
+    },
+    quantity: DataTypes.INTEGER,
+  },{
+    timestamp: false,
+    tableName: 'salesProducts',
+    underscored: true
+  });
+
+  SalesProducts.associate = (models) => {
+    models.sales.belongsToMany(models.products, {
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+      through: SalesProducts,
+      as: 'products'
+    });
+  
+    models.products.belongsToMany(models.sales, {
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+      through: SalesProducts,
+      as: 'sales'
+    });
+  }
+
+  return SalesProducts;
+};
