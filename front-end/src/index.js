77aaef44b6015d0a117e072af5f3cc137682c3e1
diegotimao <diegotimao1104@gmail.com>
2022-10-28 19:39:16 -0500

    feat: Adicionando o provider ao app

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './providers/Auth';
 import './style.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
