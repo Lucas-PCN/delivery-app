542bf0a6b29aa1d019a0c138cb32eee9e282348a
lsblu <lsblu@hotmail.com>
2022-11-01 14:04:08 -0300

    FEAT: Crud - SALES - BACK END; FIX: Corrigido criptgrafia password em CreateUser; FIX: Retorno ao criar usuario retornando Token;

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -1,21 +1,29 @@
 const { Op } = require('sequelize');
-const { users } = require('../database/models');
+const md5 = require('md5');
 
+const { users } = require('../database/models');
+const { generateToken } = require('../utils/jwt');
+
+const registered = { status: 409, message: 'user already registered' };
 const createUser = async ({ name, email, password }) => {
+  const pass = md5(password);
   const findUser = await users.findOne({ where: {
     [Op.or]: [
       { name },
       { email },
     ],
   } });
-  if (findUser) {
-    const erro = { status: 409, message: 'user aleready registered' };
-    throw erro;
-  }
+  if (findUser) throw registered;
 
-  const result = await users.create({ name, email, password, role: 'customer' });
-
-  return result;
+  const result = await users.create({ name, email, password: pass, role: 'customer' });
+  const token = generateToken(result.dataValues);
+  return {
+    id: result.id,
+    name: result.name,
+    email: result.email,
+    role: result.role, 
+    token,
+  };
 };
 
 module.exports = { createUser };
