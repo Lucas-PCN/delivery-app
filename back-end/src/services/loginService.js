0324fd16acfc2d429e52b28c3f9b2dd56f6c5345
diegotimao <diegotimao1104@gmail.com>
2022-11-01 11:02:13 -0500

    fix: Correção de erro status code LoginServices

--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -2,14 +2,16 @@ const md5 = require('md5');
 const { users } = require('../database/models');
 const { generateToken } = require('../utils/jwt');
 
-const missingFields = { status: 400, message: 'Some required fields are missing' };
-const loginIncorreto = { status: 400, message: 'Email ou senha incorreta' };
+const missingFields = { status: 404, message: 'Some required fields are missing' };
+const loginIncorreto = { status: 404, message: 'Email ou senha incorreta' };
+
 const loginUsers = async ({ email, password }) => {
   if (!email || !password) throw missingFields;
   const pass = md5(password);
   const userLogin = await users.findOne(
     { attributes: ['name', 'email', 'role'], where: { email, password: pass } },
 );
+
 if (!userLogin) throw loginIncorreto;
 
   const token = generateToken(userLogin.dataValues);
