29f01b3e9c36135abc8a85af1dcd502c51c37e8b
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-11-01 18:19:30 -0300

    Feat: corrige pequenos erros e tenta implementar create do salesProducts

--- back-end/src/api/routes.js
+++ back-end/src/api/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createCheckout } = require('../controllers/checkoutController');
 const loginController = require('../controllers/loginController');
-const productsControoler = require('../controllers/products.controller');
+const productsController = require('../controllers/products.controller');
 const registerController = require('../controllers/registerController');
 const { validateLogin, validateRegister } = require('../middlewares');
 
@@ -9,8 +9,8 @@ const route = express();
 
 route.post('/login', validateLogin, loginController.login);
 route.post('/register', validateRegister, registerController.register);
-route.post('/products', productsControoler.createProducts);
-route.get('/products', productsControoler.getProducts);
+route.post('/products', productsController.createProducts);
+route.get('/products', productsController.getProducts);
 route.post('/checkout', createCheckout);
 // A rota abaixo é um exemplo de como ir para outras rotas autenticado
 // route.get('/teste', autenticaMiddleware, (req, res, _next) => res.send('O'));
