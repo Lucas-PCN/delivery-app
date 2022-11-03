f2dde14e847a38dea5f7717cea6341723c3ae7c6
diegotimao <diegotimao1104@gmail.com>
2022-11-03 01:02:58 -0500

    feat: Criando componente header

---
+++ front-end/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <h1>Olá Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
