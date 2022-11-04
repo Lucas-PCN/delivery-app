5c73d1c56b2142d2267ba337d9452ec89b09b5bc
diegotimao <diegotimao1104@gmail.com>
2022-11-04 14:25:36 -0500

    feat: Create component header

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import './style.css';
 
 function Header() {
+  const history = useHistory();
+  const [user, setUser] = useState('');
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/login');
+  };
+
+  useEffect(() => {
+    const userLogado = JSON.parse(localStorage.getItem('user'));
+
+    if (!userLogado) {
+      return history.push('/login');
+    }
+
+    setUser(userLogado);
+  }, [history]);
+
   return (
     <div className="header-container">
       <div className="header-content">
-        <h1>Olá Header</h1>
+        <div className="btn-left">
+          <Link
+            className="btn-to-products"
+            data-testid="customer_products__element-navbar-link-products"
+            to="/customer/products"
+          >
+            Produtos
+          </Link>
+          <Link
+            className="btn-to-checkout"
+            data-testid="customer_products__element-navbar-link-orders"
+            to="/customer/checkout"
+            type="button"
+          >
+            Meus pedidos
+          </Link>
+        </div>
+        <div className="btn-rigth">
+          <h3
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            { user.name }
+          </h3>
+          <button
+            onClick={ () => logout() }
+            data-testid="customer_products__element-navbar-link-logout"
+            type="button"
+          >
+            Sair
+          </button>
+        </div>
       </div>
     </div>
   );
